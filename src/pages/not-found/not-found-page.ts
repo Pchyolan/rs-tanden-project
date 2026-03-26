f966e44dd7255967cb6cd6903f711b500c70b69b
Anna Demyanovich <makarenko-an@part-kom.ru>
2026-03-27 00:48:22 +0300

    refactor: reorganize not-found-page files

--- src/pages/not-found/not-found-page.ts
+++ src/pages/not-found/not-found-page.ts
@@ -5,9 +5,102 @@ import { createBackArrow } from '@/utils/svg-icon.ts';
 import notFoundImageUrl from '@/assets/images/brains/not-found.png';
 
 import './not-found-page.scss';
+import './curved-text.scss';
 
 export function notFoundPage(navigate: (path: string) => void): Page {
   let component: BaseComponent;
+
+  /**
+   * Создаёт кнопку возврата на главную.
+   */
+  const createReturnButton = (): BaseComponent<'button'> => {
+    const button = new BaseComponent({
+      tag: 'button',
+      className: ['not-found-page__button'],
+    });
+
+    const buttonContent = new BaseComponent({
+      tag: 'span',
+      className: ['button-content'],
+    });
+
+    const arrowIcon = createBackArrow();
+    const arrowWrapper = new BaseComponent({
+      tag: 'span',
+      className: ['button-arrow'],
+    });
+    arrowWrapper.element.append(arrowIcon);
+
+    const buttonText = new BaseComponent({
+      tag: 'span',
+      text: 'Go Home',
+    });
+
+    buttonContent.append(arrowWrapper, buttonText);
+    button.append(buttonContent);
+    button.addEventListener('click', () => navigate('/'));
+
+    return button;
+  };
+
+  /**
+   * Создаёт изогнутый текст "404".
+   */
+  const createCurvedText = (): BaseComponent<'div'> => {
+    const curvedText = new BaseComponent({
+      tag: 'div',
+      className: ['curved-text'],
+    });
+
+    ['4', '0', '4'].forEach((digit, index) => {
+      const span = new BaseComponent({
+        tag: 'span',
+        text: digit,
+        className: ['curved-text__digit', `digit-${index}`],
+      });
+      curvedText.append(span);
+    });
+
+    return curvedText;
+  };
+
+  /**
+   * Создаёт анимационную обёртку с текстом и изображением.
+   */
+  const createAnimationWrapper = (): BaseComponent<'div'> => {
+    const wrapper = new BaseComponent({
+      tag: 'div',
+      className: ['animation-wrapper'],
+    });
+
+    const image = new BaseComponent<'img'>({
+      tag: 'img',
+      className: ['not-found-page__image'],
+      attrs: { src: notFoundImageUrl, alt: 'Not found image' },
+    });
+
+    wrapper.append(createCurvedText(), image);
+    return wrapper;
+  };
+
+  /**
+   * Создаёт нижнюю часть страницы с текстом и кнопкой.
+   */
+  const createButtonWrapper = (): BaseComponent<'div'> => {
+    const wrapper = new BaseComponent({
+      tag: 'div',
+      className: ['button-wrapper'],
+    });
+
+    const message = new BaseComponent({
+      tag: 'p',
+      text: 'Oh no! Page not found',
+      className: ['not-found-page__text'],
+    });
+
+    wrapper.append(message, createReturnButton());
+    return wrapper;
+  };
 
   return {
     render() {
@@ -16,72 +109,7 @@ export function notFoundPage(navigate: (path: string) => void): Page {
         className: ['not-found-page'],
       });
 
-      const returnButton = new BaseComponent({
-        tag: 'button',
-        className: ['not-found-page__button'],
-      });
-
-      const buttonContent = new BaseComponent({
-        tag: 'span',
-        className: ['button-content'],
-      });
-
-      const arrowIcon = createBackArrow();
-      const arrowWrapper = new BaseComponent({ tag: 'span', className: ['button-arrow'] });
-      arrowWrapper.element.append(arrowIcon);
-
-      const buttonText = new BaseComponent({
-        tag: 'span',
-        text: 'Go Home',
-      });
-
-      buttonContent.append(arrowWrapper, buttonText);
-      returnButton.append(buttonContent);
-      returnButton.addEventListener('click', () => navigate('/'));
-
-      const curvedText = new BaseComponent({
-        tag: 'div',
-        className: ['curved-text'],
-      });
-
-      ['4', '0', '4'].forEach((digit, index) => {
-        const span = new BaseComponent({
-          tag: 'span',
-          text: digit,
-          className: ['curved-text__digit', `digit-${index}`],
-        });
-        curvedText.append(span);
-      });
-
-      const animationWrapper = new BaseComponent({
-        tag: 'div',
-        className: ['animation-wrapper'],
-      });
-
-      animationWrapper.append(
-        curvedText,
-        new BaseComponent<'img'>({
-          tag: 'img',
-          className: ['not-found-page__image'],
-          attrs: { src: notFoundImageUrl, alt: 'Not found image' },
-        })
-      );
-
-      const buttonWrapper = new BaseComponent({
-        tag: 'div',
-        className: ['button-wrapper'],
-      });
-      buttonWrapper.append(
-        new BaseComponent({
-          tag: 'p',
-          text: 'Oh no! Page not found',
-          className: ['not-found-page__text'],
-        }),
-        returnButton
-      );
-
-      component.append(animationWrapper, buttonWrapper);
-
+      component.append(createAnimationWrapper(), createButtonWrapper());
       return component;
     },
     onMount() {
